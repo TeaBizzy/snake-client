86cdbfe8da4af680c898e872e13fa9c9517acfe3
Stefan <satalbot@protonmail.com>
2022-10-26 13:03:01 +0000

    Refactored connection establishing to client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// Establishes a connection with the game server
+const connect = function(ipAddress = "localhost") {
+  if(!ipAddress) {
+    ipAddress = "localhost";
+  }
+
+  const conn = net.createConnection({
+    host: ipAddress, // IP Address
+    port: 50541        // PORT number here
+  });
+
+  // Give connection status to user
+  console.log(`Connecting to IP: ${ipAddress}, on PORT: 50541`);
+
+  // Interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Displays messages from the server
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+
+
+module.exports = { connect };
