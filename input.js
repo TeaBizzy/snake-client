7976407e252aeb147f09edcc7423e5f091018a62
Stefan <satalbot@protonmail.com>
2022-10-26 14:11:08 +0000

    Implemented player movement

--- input.js
+++ input.js
@@ -1,21 +1,40 @@
+let connection; // Stores our connection to the server so we can send it data from our inputs.
+
 // Initializes capturing of user input
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume;
 
+  // Setup the connection reference
+  connection = conn
+
   // Register for the "data" event
   stdin.on("data", handleUserInput);
+
 
   return stdin;
 };
 
+// Moves the player in the given direction
+const movePlayer = function(direction) {
+  // If input given isn't valid just return!
+  if (direction !== "w" && direction !== "a" && direction !== "s" && direction !== "d") {
+    return;
+  }
+
+  connection.write(`Move: ${direction}`); // Tell the server to move our snake!
+};
+
+// Determines what to do with user input
 const handleUserInput = function(key) {
+  // If input is = "ctrl + c" kill the application.
   if (key === "\u0003") {
     process.exit();
   }
-};
 
+  movePlayer(key);
+};
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 const args = process.argv.slice(2);
 
-const stdin = setupInput(); // Stores the stdin
 const conn = connect(args[0]); // Stores the conn
+const stdin = setupInput(conn); // Stores the stdin, input needs access to the connection to handle inputs properly
